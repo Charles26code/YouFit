73513c5ae37782bfe6ac1f1d1741c274dccc7c90
Sofiane RADJAH <55430903+Sofrad83@users.noreply.github.com>
2022-05-25 14:38:27 +0200

    refonte login ok

--- youfit/flutter/lib/screen/Login.dart
+++ youfit/flutter/lib/screen/Login.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -12,40 +13,30 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: const [
-          Background(),
-          LogoSection(),
-          LoginText(),
-          LoginSection(),
-          QuestionSection(),
+        children:  [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              LogoSection(),
+              LoginSection(),
+              QuestionSection(),
+            ],
+          ),
+          Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('img/bg-circle.png'),
+              alignment: FractionalOffset.topCenter,
+            ),
+          ),
+        )
         ],
       ),
     );
   }
 }
-
-// class Background extends StatelessWidget {
-//   const Background({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ShaderMask(
-//       shaderCallback: (bounds) => const LinearGradient(
-//               colors: [Colors.black, Colors.black12],
-//               begin: Alignment.bottomCenter,
-//               end: Alignment.center)
-//           .createShader(bounds),
-//       blendMode: BlendMode.darken,
-//       child: Container(
-//           decoration: const BoxDecoration(
-//               image: DecorationImage(
-//                   image: AssetImage('img/fitness.jpg'),
-//                   fit: BoxFit.cover,
-//                   colorFilter:
-//                       ColorFilter.mode(Colors.black45, BlendMode.darken)))),
-//     );
-//   }
-// }
 
 class Background extends StatelessWidget {
   const Background({Key? key}) : super(key: key);
@@ -57,10 +48,10 @@ class Background extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
-            fit: BoxFit.fill,
+            fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.2), BlendMode.dstATop),
-            image: const AssetImage('img/fitness.jpg'),
+                Colors.black.withOpacity(0.5), BlendMode.dstATop),
+            image: const AssetImage('img/bg-login.jpg'),
             alignment: FractionalOffset.topCenter,
           ),
         ),
@@ -76,71 +67,45 @@ class LogoSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
       Padding(
-          padding: const EdgeInsets.only(top: 50),
+          padding: const EdgeInsets.only(top: 150),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                // padding: const EdgeInsets.only(top: 30),
-                child: Image.asset(
-                  "img/peach.png",
-                  height: 100,
-                  width: 100,
-                  //color: Color.fromARGB(1000, 238, 122, 43),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: const Text('Fit',
+                child: const Text('YOU',
                     textAlign: TextAlign.left,
                     style: TextStyle(
                         color: Colors.white,
-                        fontFamily: 'Abel-Regular',
-                        fontSize: 50)),
+                        fontFamily: 'Koulen',
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
               ),
-              Expanded(
-                flex: 0,
-                child: Container(
-                  padding: const EdgeInsets.only(top: 8),
-                  height: 80,
-                  width: 80,
-                  // padding: const EdgeInsets.only(top: 80),
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(1000, 238, 122, 43),
-                      borderRadius: BorderRadius.circular(20)),
-                  //padding: const EdgeInsets.all(10),
-                  child: const Text('hub',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Abel-Regular',
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold)),
+              Container(
+                child: const Text('FIT',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Color.fromARGB(1000, 0, 232, 51),
+                        fontFamily: 'Koulen',
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+              ),
+              Container(
+                child: RotationTransition(
+                  turns: const AlwaysStoppedAnimation(135 / 360),
+                  child: SvgPicture.asset(
+                    "img/dumbbell.svg",
+                    color: const Color.fromARGB(1000, 0, 232, 51),
+                    width: 35,
+                  ),
                 ),
-              )
+              ),
             ],
           )),
     ]);
-  }
-}
-
-class LoginText extends StatelessWidget {
-  const LoginText({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 200, left: 10),
-      child: const Text(
-        'Sign In ',
-        textAlign: TextAlign.left,
-        style: TextStyle(
-            color: Color.fromARGB(1000, 238, 122, 43),
-            fontFamily: 'Rock3D-Regular',
-            fontSize: 35,
-            fontWeight: FontWeight.bold),
-      ),
-    );
   }
 }
 
@@ -152,7 +117,6 @@ class LoginSection extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const SizedBox(height: 20),
         Padding(
           padding: const EdgeInsets.only(left: 50, right: 50),
           child: Column(
@@ -161,19 +125,20 @@ class LoginSection extends StatelessWidget {
                   flex: 0,
                   child: Container(
                     decoration: BoxDecoration(
-                      gradient: const LinearGradient(colors: [
-                        Colors.white,
-                        Color.fromARGB(1000, 238, 122, 43)
+                      gradient: LinearGradient(colors: [
+                        Colors.white.withOpacity(0.7),
+                        Colors.white.withOpacity(0.7)
                       ]),
-                      borderRadius: BorderRadius.circular(30),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: const TextField(
+                    child: TextField(
                       obscureText: false,
                       decoration: InputDecoration(
-                          hintText: 'Username',
-                          suffixIcon: Icon(Icons.email),
-                          contentPadding: EdgeInsets.only(left: 10, top: 14),
-                          border: InputBorder.none),
+                          hintText: 'Adresse Mail ou Pseudo',
+                          suffixIcon: Icon(Icons.email, color: Colors.white.withOpacity(0),),
+                          contentPadding: const EdgeInsets.only(left: 10, top: 14),
+                          border: InputBorder.none
+                        ),
                     ),
                   )),
               const SizedBox(height: 20),
@@ -181,36 +146,56 @@ class LoginSection extends StatelessWidget {
                 flex: 0,
                 child: Container(
                   decoration: BoxDecoration(
-                    gradient: const LinearGradient(colors: [
-                      Colors.white,
-                      Color.fromARGB(1000, 238, 122, 43)
+                    gradient: LinearGradient(colors: [
+                      Colors.white.withOpacity(0.7),
+                      Colors.white.withOpacity(0.7)
                     ]),
-                    borderRadius: BorderRadius.circular(30),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: const TextField(
+                  child: TextField(
                     obscureText: true,
                     decoration: InputDecoration(
-                        hintText: 'Password',
-                        suffixIcon: Icon(Icons.visibility_off),
-                        contentPadding: EdgeInsets.only(left: 10, top: 14),
-                        border: InputBorder.none),
+                        hintText: 'Mot de passe',
+                        suffixIcon: Icon(Icons.visibility_off, color: Colors.white.withOpacity(0),),
+                        contentPadding: const EdgeInsets.only(left: 10, top: 14),
+                        border: InputBorder.none,
+                      ),
                   ),
                 ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => {}, 
+                    child: const Text(
+                      'Mot de passe oublié ?',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'PT Sans',
+                          fontSize: 13),
+                    ))
+                ],
               ),
               const SizedBox(
                 height: 20,
               ),
-              FloatingActionButton.extended(
-                backgroundColor: const Color.fromARGB(1000, 238, 122, 43),
-                hoverColor: Colors.deepOrangeAccent,
-                onPressed: () {},
-                label: const Text(
-                  'Sign In',
-                  style: TextStyle(
-                    fontFamily: 'Abel-Regular',
+              SizedBox(
+                width: 325,
+                child: FloatingActionButton.extended(
+                  backgroundColor: const Color.fromARGB(1000, 0, 232, 51),
+                  hoverColor: Color.fromARGB(255, 0, 255, 55),
+                  onPressed: () {},
+                  label: const Text(
+                    'CONNEXION',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontFamily: 'PT Sans',
+                    ),
                   ),
                 ),
               ),
+              
             ],
           ),
         ),
@@ -226,27 +211,23 @@ class QuestionSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         child: Padding(
-      padding: const EdgeInsets.only(top: 600),
-      child: Row(
+          padding:  const EdgeInsets.only(bottom: 400), 
+          child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Don\'t have any account ?',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white, fontFamily: 'Abel-Regular', fontSize: 10),
-          ),
           TextButton(
               onPressed: () {},
               child: const Text(
-                'CREATE ONE',
+                'Créer un compte ->',
                 style: TextStyle(
-                    color: Color.fromARGB(1000, 238, 122, 43),
-                    fontFamily: 'Abel-Regular',
-                    fontSize: 10),
+                    color: Colors.white,
+                    fontFamily: 'PT Sans',
+                    fontSize: 13),
               )),
         ],
       ),
-    ));
+        )
+        ,
+    );
   }
 }
